3992fa47d4d31ed77de12cb0344e2ecc48881d6f
RamkumarV09 <ramkumarv0986@gmail.com>
2025-03-06 19:16:17 +0530

    Code

---
+++ src/test/java/org/sample/SampleGITRev.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+public class SampleGITRev {
+	
+	public void tc1() {
+
+
+		System.out.println("TC1");
+
+	}
+	
+	public void tc2() {
+
+
+		System.out.println("TC2");
+
+	}
+	
+	
+
+}
